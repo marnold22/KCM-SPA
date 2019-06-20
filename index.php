8f14099aa0be418bc5aeeb437f5838143927f01c
marnold22 <minimearnold@gmail.com>
2019-06-20 15:26:09 -0700

    removed header and nav from index to use as modules

--- index.php
+++ index.php
@@ -15,20 +15,16 @@ exit;
   <meta name="description" content="CPPS LMS for KCM">
   <meta name="" content="">
 
+  <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="css/card-flip.css">
   <link rel="stylesheet" href="css/main.css">
 </head>
 
 <body>
+  <?php include 'header/navigation_login.php'; ?>
+  <?php include 'header/header.php'; ?>
 
-  <div class="container-fluid">
-      <div class="text-center">
-          <p class="welcomeTitle">WELCOME</p>
-          <img src="../assets/images/KCMLogo.png" alt="..." style="" class="KCMLogo">
-      </div>
-  </div>
-  <br>
   <div class="container-fluid descriptionBanner">
       <div class="text-center">
           <p class="descriptionContent">CPPS is the leading developer and provider of scalable training and consulting solutions in the U.S. for Workplace Violence Prevention, Active Shooter Response, and International Travel Safety. CPPS has worked together with thousands of organizations–large and small–to include over 50% of Fortune 100 corporations, over 1600 colleges and universities; 2000 hospitals and many of the largest non-profit/charitable organizations in the U.S.</p>
@@ -36,8 +32,8 @@ exit;
   </div>
 
   <div class="wrapper">
+
     <?php
-      // -- card_flip
       //Include the When Lightning Strikes Module
       include 'courseCards/WLS_Module.php';
       //Include the FPDP Module
@@ -47,14 +43,12 @@ exit;
       //Include the Beyond Lockdown Module
       include 'courseCards/BL_Module.php';
     ?>
-  </div>
 
-  <div class="wrapper">
-    <a href="logout.php">Logout</a>
   </div>
-
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
