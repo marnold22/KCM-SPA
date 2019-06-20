fca69b4f41cf10d083e2cf9f66033e308927c344
marnold22 <minimearnold@gmail.com>
2019-06-20 16:39:15 -0700

    Created login page and styled

--- login.php
+++ login.php
@@ -1,49 +1,63 @@
-<form action="" method="post" name="Login_Form">
-  <table width="400" border="0" align="center" cellpadding="5" cellspacing="1" class="Table">
-    <?php if(isset($msg)){?>
-    <tr>
-      <td colspan="2" align="center" valign="top"><?php echo $msg;?></td>
-    </tr>
-    <?php } ?>
-    <tr>
-      <td colspan="2" align="left" valign="top"><h3>Login</h3></td>
-    </tr>
-    <tr>
-      <td align="right" valign="top">Username</td>
-      <td><input name="Username" type="text" class="Input"></td>
-    </tr>
-    <tr>
-      <td align="right">Password</td>
-      <td><input name="Password" type="password" class="Input"></td>
-    </tr>
-    <tr>
-      <td> </td>
-      <td><input name="Submit" type="submit" value="Login" class="Button3"></td>
-    </tr>
-  </table>
-</form>
+<head>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script type="text/javascript">
+      $(window).on('load',function(){
+          $('#exampleModalCenter').modal('show');
+      });
+  </script>
+</head>
 
+<!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Login</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="" method="post" name="Login_Form">
+          <?php if(isset($msg)){?>
+            <p><?php echo $msg;?></p>
+          <?php } ?>
+          <label>Username</label><br>
+          <input name="Username" type="text" class="Input"><br>
+          <label>Password</label><br>
+          <input name="Password" type="password" class="Input"><br><br>
+          <input name="Submit" type="submit" value="Login" class="btn btn-secondary">
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- ++++++++++++++++++++++++++++++++++++ PHP STUFF HERE ++++++++++++++++++++++++++++++++++++ -->
 
-<?php session_start(); /* Starts the session */
+<?php
+session_start(); /* Starts the session */
 /* Check Login form submitted */
 if(isset($_POST['Submit'])){
-  /* Define username and associated password array */
-  $logins = array('CPPS' => 'CPPSdev1!');
+/* Define username and associated password array */
+$logins = array('CPPS' => 'CPPSdev1!');
 
-  /* Check and assign submitted Username and Password to new variable */
-  $Username = isset($_POST['Username']) ? $_POST['Username'] : '';
-  $Password = isset($_POST['Password']) ? $_POST['Password'] : '';
+/* Check and assign submitted Username and Password to new variable */
+$Username = isset($_POST['Username']) ? $_POST['Username'] : '';
+$Password = isset($_POST['Password']) ? $_POST['Password'] : '';
 
-  /* Check Username and Password existence in defined array */
-  if (isset($logins[$Username]) && $logins[$Username] == $Password){
-    /* Success: Set session variables and redirect to Protected page  */
-    $_SESSION['UserData']['Username']=$logins[$Username];
-    header("location:index.php");
-    exit;
-  }else {
-    /*Unsuccessful attempt: Set error message */
-    $msg="<span style='color:red'>Invalid Login Details</span>";
-    echo $msg;
-  }
+/* Check Username and Password existence in defined array */
+if (isset($logins[$Username]) && $logins[$Username] == $Password){
+/* Success: Set session variables and redirect to Protected page  */
+$_SESSION['UserData']['Username']=$logins[$Username];
+header("location:index.php");
+exit;
+} else {
+/*Unsuccessful attempt: Set error message */$msg="<span style='color:red'>Invalid Login Details</span>";
+}
 }
 ?>
