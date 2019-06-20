9ce43bfa1952203d97bc1e4923dc11c3a7ac3d57
marnold22 <minimearnold@gmail.com>
2019-06-20 15:25:36 -0700

    added header and navigation as modules

---
+++ header/header.php
@@ -0,0 +1,7 @@
+<div class="container-fluid">
+    <div class="text-center">
+        <p class="welcomeTitle">WELCOME</p>
+        <img src="../assets/images/KCMLogo.png" alt="..." style="" class="KCMLogo">
+    </div>
+</div>
+<br>
---
+++ header/navigation_login.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <a class="navbar-brand" href="index.php"><img class="img-responsive" src="../assets/images/cpps_university_logo.png" alt="" /></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto mr-1">
+        <li class="nav-item">
+          <a class="nav-link" href="#"></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link"  href="../logout.php"><i class="fa fa-power-off"></i> Logout</a>
+        </li>
+      </ul>
+    </div>
+</nav>
