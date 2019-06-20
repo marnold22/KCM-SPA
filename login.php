2c1c8c308f4e3ba862dfc29a1cfd09d676df6e59
marnold22 <minimearnold@gmail.com>
2019-06-20 12:23:07 -0700

    Updated login page

--- login.php
+++ login.php
@@ -1,47 +1,66 @@
-<form action="" method="post" name="Login_Form">
-<table width="400" border="0" align="center" cellpadding="5" cellspacing="1" class="Table">
-  <?php if(isset($msg)){?>
-  <tr>
-    <td colspan="2" align="center" valign="top"><?php echo $msg;?></td>
-  </tr>
-  <?php } ?>
-  <tr>
-    <td colspan="2" align="left" valign="top"><h3>Login</h3></td>
-  </tr>
-  <tr>
-    <td align="right" valign="top">Username</td>
-    <td><input name="Username" type="text" class="Input"></td>
-  </tr>
-  <tr>
-    <td align="right">Password</td>
-    <td><input name="Password" type="password" class="Input"></td>
-  </tr>
-  <tr>
-    <td> </td>
-    <td><input name="Submit" type="submit" value="Login" class="Button3"></td>
-  </tr>
-</table>
-</form>
+<!doctype html>
 
-<?php session_start(); /* Starts the session */
-/* Check Login form submitted */
-if(isset($_POST['Submit'])){
-  /* Define username and associated password array */
-  $logins = array('Mike' => 'CPPSdev1!');
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
 
-  /* Check and assign submitted Username and Password to new variable */
-  $Username = isset($_POST['Username']) ? $_POST['Username'] : '';
-  $Password = isset($_POST['Password']) ? $_POST['Password'] : '';
+    <title>Kennith Copeland Ministries</title>
+    <meta name="description" content="CPPS LMS for KCM">
+    <meta name="" content="">
 
-  /* Check Username and Password existence in defined array */
-  if (isset($logins[$Username]) && $logins[$Username] == $Password){
-    /* Success: Set session variables and redirect to Protected page  */
-    $_SESSION['UserData']['Username']=$logins[$Username];
-    header("location:index.php");
-    exit;
-  } else {
-    /*Unsuccessful attempt: Set error message */
-    $msg="<span style='color:red'>Invalid Login Details</span>";
-  }
-}
-?>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/card-flip.css">
+    <link rel="stylesheet" href="css/main.css">
+  </head>
+
+  <body>
+    <form action="" method="post" name="Login_Form">
+    <table width="400" border="0" align="center" cellpadding="5" cellspacing="1" class="Table">
+      <?php if(isset($msg)){?>
+      <tr>
+        <td colspan="2" align="center" valign="top"><?php echo $msg;?></td>
+      </tr>
+      <?php } ?>
+      <tr>
+        <td colspan="2" align="left" valign="top"><h3>Login</h3></td>
+      </tr>
+      <tr>
+        <td align="right" valign="top">Username</td>
+        <td><input name="Username" type="text" class="Input"></td>
+      </tr>
+      <tr>
+        <td align="right">Password</td>
+        <td><input name="Password" type="password" class="Input"></td>
+      </tr>
+      <tr>
+        <td> </td>
+        <td><input name="Submit" type="submit" value="Login" class="Button3"></td>
+      </tr>
+    </table>
+    </form>
+
+    <?php session_start(); /* Starts the session */
+    /* Check Login form submitted */
+    if(isset($_POST['Submit'])){
+      /* Define username and associated password array */
+      $logins = array('CPPS' => 'CPPSdev1!');
+
+      /* Check and assign submitted Username and Password to new variable */
+      $Username = isset($_POST['Username']) ? $_POST['Username'] : '';
+      $Password = isset($_POST['Password']) ? $_POST['Password'] : '';
+
+      /* Check Username and Password existence in defined array */
+      if (isset($logins[$Username]) && $logins[$Username] == $Password){
+        /* Success: Set session variables and redirect to Protected page  */
+        $_SESSION['UserData']['Username']=$logins[$Username];
+        header("location:index.php");
+        exit;
+      } else {
+        /*Unsuccessful attempt: Set error message */
+        $msg="<span style='color:red'>Invalid Login Details</span>";
+      }
+    }
+    ?>
+
+  </body>
+</html>
